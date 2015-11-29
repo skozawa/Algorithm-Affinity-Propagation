74fe784f3a8921186188dbc86fb7d309d5fb75e1
skozawa <skozawa@hatena.ne.jp>
2015-11-29 22:41:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Algorithm::Affinity::Propagation - It's new $module
+
+# SYNOPSIS
+
+    use Algorithm::Affinity::Propagation;
+
+# DESCRIPTION
+
+Algorithm::Affinity::Propagation is ...
+
+# LICENSE
+
+Copyright (C) skozawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+skozawa &lt;skozawa@hatena.ne.jp>
---
+++ lib/Algorithm/Affinity/Propagation.pm
@@ -0,0 +1,39 @@
+package Algorithm::Affinity::Propagation;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Algorithm::Affinity::Propagation - It's new $module
+
+=head1 SYNOPSIS
+
+    use Algorithm::Affinity::Propagation;
+
+=head1 DESCRIPTION
+
+Algorithm::Affinity::Propagation is ...
+
+=head1 LICENSE
+
+Copyright (C) skozawa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+skozawa E<lt>skozawa@hatena.ne.jpE<gt>
+
+=cut
+
